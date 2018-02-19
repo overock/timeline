a3f4f46375adf001bd499fdfed64bd2de7c6e8a8
Roaster Kay <overock@daum.net>
2018-02-19 18:06:07 +0900

    no message

--- js/timeline.js
+++ js/timeline.js
@@ -4,11 +4,27 @@ class Timeline {
     this._data = null;
   }
 
-  set data(d) { this._data = d; }
+  set data(d) {
+    this._data = new Proxy(d, {
+      set: (obj, prop, value) => {
+        obj[prop] = value;
+        this.render();
+      },
+
+      deleteProperty: (obj, prop) => {
+        delete obj[prop];
+        this.render();
+      }
+    });
+
+    this.render();
+  }
   get data() { return this._data; }
 
   render() {
-
+    // axis 분석(변량)
+    // 눈금 세팅 (초, 분, 시, 일)
+    // 적절히 그리기
   }
 
   append(el) {
